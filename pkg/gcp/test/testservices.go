d9e2b69f4fecd60605a794f68350429063ff4efc
Andreas Bergmeier <andreas.bergmeier@otto.de>
2022-09-08 14:48:50 +0200

    Fix tests for Services

--- pkg/gcp/test/testservices.go
+++ pkg/gcp/test/testservices.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/otto-de/sherlock-microservice/pkg/gcp"
+	"go.opentelemetry.io/otel/sdk/trace"
 )
 
 type TestServices struct {
@@ -14,11 +15,13 @@ type TestServices struct {
 func NewTestServices(t *testing.T, serviceName string) *TestServices {
 	er, es := newTestErrorReportingClient(t, serviceName)
 	lc := newTestLoggingClient(t)
+	tp := trace.NewTracerProvider(trace.WithSampler(trace.NeverSample()))
 
 	return &TestServices{
 		Services: gcp.Services{
 			ErrorReporting: er,
 			Logging:        lc,
+			TracerProvider: tp,
 		},
 		TestErrorReportingService: es,
 	}
