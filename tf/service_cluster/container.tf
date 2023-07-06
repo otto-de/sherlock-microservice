61539d8564fc6b3bde4d7a9236ee4cbb533cd638
Andreas Bergmeier <51448674+AndreasBergmeier6176@users.noreply.github.com>
2023-07-06 10:20:15 +0200

    Remove KEDA from container clusters

--- tf/service_cluster/container.tf
+++ tf/service_cluster/container.tf
@@ -21,9 +21,7 @@ resource "google_container_cluster" "main" {
 
   node_pool_auto_config {
     network_tags {
-      tags = concat([
-        "gke-keda-metrics",
-      ], var.add_node_pool_network_tags)
+      tags = var.add_node_pool_network_tags
     }
   }
 
@@ -34,29 +32,5 @@ resource "google_container_cluster" "main" {
     master_global_access_config {
       enabled = true
     }
-  }
-}
-
-resource "google_compute_firewall" "keda_allow_master" {
-  name        = "gke-${var.name}-keda-metric-server"
-  description = "KEDA Metric Server: A firewall rule to allow worker nodes communication with master"
-  network     = var.network.name
-  priority    = 1000
-  direction   = "INGRESS"
-
-  source_ranges = [
-    var.master_cidr_block,
-  ]
-  target_tags = [
-    "gke-keda-metrics",
-  ]
-
-  allow {
-    protocol = "tcp"
-    ports    = ["6443"]
-  }
-
-  log_config {
-    metadata = "INCLUDE_ALL_METADATA"
   }
 }
