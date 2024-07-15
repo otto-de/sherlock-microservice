b88e3be08b413bf3fdd41ac6075b042bf5b881eb
Andreas Bergmeier <andreas.bergmeier@otto.de>
2024-07-15 15:33:06 +0200

    Enforce Docker file paths to building Containers

--- pkg/dockerimage/build.go
+++ pkg/dockerimage/build.go
@@ -3,19 +3,18 @@ package dockerimage
 import (
 	"io"
 	"os"
-	"path/filepath"
 )
 
-func BuildFromContainerfileWithWriters(out io.Writer, errOut io.Writer, path string, tag string) error {
+func BuildFromContainerfileWithWriters(out io.Writer, errOut io.Writer, dockerFilePath, contextPath, tag string) error {
 	// Normally you would want to use a Golang API here.
 	// Since the Docker API is a very thin shim, badly documented
 	// and talking to Dockerd or Buildkit has a lot of quirks
 	// just call the binary here and be done with it.
-	return executeDockerCommandWithWriters(out, errOut, "build", "-f", filepath.Join(path, "Containerfile"), path, "-t", tag)
+	return executeDockerCommandWithWriters(out, errOut, "build", "-f", dockerFilePath, contextPath, "-t", tag)
 }
 
 // BuildFromContainerfile builds a Container image from a Containerfile.
 // Invokes docker via CLI since Docker's dependencies and API are beyond brittle.
-func BuildFromContainerfile(path string, tag string) error {
-	return BuildFromContainerfileWithWriters(os.Stdout, os.Stderr, path, tag)
+func BuildFromContainerfile(dockerFilePath, contextPath, tag string) error {
+	return BuildFromContainerfileWithWriters(os.Stdout, os.Stderr, dockerFilePath, contextPath, tag)
 }
