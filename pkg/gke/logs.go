b2ac8f7537527b59723367649a8404976a95cfe3
JonasR <jonas.reetz@otto.de>
2024-10-15 15:26:57 +0200

    Make pod log streaming respect the connection settings of the clientset

--- pkg/gke/logs.go
+++ pkg/gke/logs.go
@@ -1,44 +1,41 @@
 package gke
 
 import (
-	"strings"
+	"context"
+	"io"
 	"time"
 
 	core "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
-	"k8s.io/kubectl/pkg/cmd/logs"
-	"k8s.io/kubectl/pkg/polymorphichelpers"
+	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
 )
 
 var defaultConfigFlags = genericclioptions.NewConfigFlags(true).WithDeprecatedPasswordFlag().WithDiscoveryBurst(300).WithDiscoveryQPS(50.0)
 
 // StreamContainerLog streams log for that one container to Stdout and Stderr.
-func StreamContainerLog(namespace string, pod *core.Pod, containerName string, streams genericclioptions.IOStreams) error {
-
-	lo := logs.NewLogsOptions(streams, false)
-	lo.Follow = true
-	lo.Container = containerName
-	lo.Namespace = namespace
-	lo.ConsumeRequestFn = logs.DefaultConsumeRequest
-	lo.RESTClientGetter = defaultConfigFlags
-	lo.Object = pod
-	lo.LogsForObject = polymorphichelpers.LogsForObjectFn
-	var err error
-	lo.Options, err = lo.ToLogOptions()
+func StreamContainerLog(ctx context.Context, pods v1.PodInterface, pod *core.Pod, containerName string, streams genericclioptions.IOStreams) error {
+	// wait for pod to be running or terminated
+	// otherwise we cant get logs
+	for {
+		spod, err := pods.Get(ctx, pod.Name, metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
+		if spod.Status.Phase == core.PodRunning || spod.Status.Phase == core.PodSucceeded || spod.Status.Phase == core.PodFailed {
+			break
+		}
+		time.Sleep(500 * time.Millisecond)
+	}
+	r := pods.GetLogs(pod.Name, &core.PodLogOptions{Follow: true, Container: containerName})
+	rc, err := r.Stream(ctx)
 	if err != nil {
 		return err
 	}
-	err = lo.Validate()
-	for err == nil {
-		err = lo.RunLogs()
-		if err == nil {
-			return err
-		}
-		if strings.HasSuffix(err.Error(), "is waiting to start: ContainerCreating") {
-			err = nil
-			time.Sleep(time.Millisecond * 100)
-		}
+	defer rc.Close()
+	_, err = io.Copy(streams.Out, rc)
+	if err != nil {
+		return err
 	}
-
-	return err
+	return nil
 }
