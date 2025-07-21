f7014f91086da325165e337a0538c34186ce4e56
Andreas Bergmeier <andreas.bergmeier@otto.de>
2025-07-21 13:56:15 +0200

    Expose bucket URL

--- tf/bucket/outputs.tf
+++ tf/bucket/outputs.tf
@@ -2,3 +2,8 @@ output "name" {
   description = "The name of the bucket."
   value       = google_storage_bucket.main.name
 }
+
+output "url" {
+  description = "The URL of the bucket."
+  value       = google_storage_bucket.main.url
+}
